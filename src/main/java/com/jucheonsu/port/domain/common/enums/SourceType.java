51cb0a36b14fb09d6c0d6bb14f714f6d1e8d9299
jucheonsu <jucheonsu@gmail.com>
2026-06-14 17:20:49 +0900

    [Refactor] SourceType 항목 정리 및 dummy.sql 파일 제거"

--- src/main/java/com/jucheonsu/port/domain/common/enums/SourceType.java
+++ src/main/java/com/jucheonsu/port/domain/common/enums/SourceType.java
@@ -2,11 +2,6 @@ package com.jucheonsu.port.domain.common.enums;
 
 public enum SourceType {
     DIRECT,
-    GITHUB,
-    FIGMA,
-    TWITTER,
-    BEHANCE,
-    DRIBBBLE,
     SEARCH,
     OTHER
 }
